54b8a42ebe67edbec9a42904dd8899a19ece2b4e
Felipe Silva <joaofelipems21@gmail.com>
2024-01-20 06:02:26 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -20,7 +20,7 @@ import jakarta.persistence.Table;
 // Nomeia o nome da tabela de Order para tb_order
 @Table(name = "tb_order")
 public class Order implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;
 
 	// Indentifica quem vai ser o ID
